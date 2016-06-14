4c0cc4e74244793f269cb3d7a59474192e47aa0d
Eike Hirsch <eike.hirsch@smartsteuer.de>
2016-06-14 16:45:14 +0200

    adds working live reload and initial debug page

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp         = require('gulp'),
     sass         = require('gulp-sass'),
     sourcemaps   = require('gulp-sourcemaps'),
+	livereload   = require('gulp-livereload'),
     gls          = require('gulp-live-server'),
     watch        = require('gulp-watch'),
     autoprefixer = require('gulp-autoprefixer');
@@ -8,26 +9,26 @@ var gulp         = require('gulp'),
 // @task sass
 gulp.task('sass', function () {
   return gulp
-    .src('app/scss/**/*.scss')
-    .pipe(watch('app/scss/**/*.scss'))
+    .src('app/stylesheets/**/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(sourcemaps.write())
     .pipe(autoprefixer({
       browsers: ['last 2 versions']
     }))
-    .pipe(gulp.dest('public/stylesheets/'));
+    .pipe(gulp.dest('public/stylesheets/'))
+    .pipe(livereload());
 });
 
 gulp.task('js', function () {
   return gulp.src('app/js/**/*.js')
-		  .pipe(watch('app/js/**/*.js'))
-		  .pipe(gulp.dest('public/js'));
+		  .pipe(gulp.dest('public/js'))
+		  .pipe(livereload());
 });
 
 // @task serve
 gulp.task('serve', function() {
-    var server = gls('app.js');
+    var server = gls.new('bin/www.js');
     server.start('./bin/www').then(function(result) {
         console.log('Server exited with result:', result);
         process.exit(result.code);
@@ -35,7 +36,11 @@ gulp.task('serve', function() {
     gulp.watch(['public/**/*.*'], function(file) {
         server.notify.apply(server, [file]);
     });
-    gulp.watch('app.js', server.start);
+});
+
+gulp.task('watch', ['js', 'sass'], function() {
+	gulp.watch('app/js/**/*.js', ['js']);
+	gulp.watch('app/stylesheets/**/*.scss', ['sass']);
 });
 
 gulp.task('default', [
@@ -43,7 +48,5 @@ gulp.task('default', [
 ]);
 
 gulp.task('play', [
-  'serve',
-  'js',
-  'sass'
+  'serve', 'watch'
 ]);
