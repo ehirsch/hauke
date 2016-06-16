60ad20fe6e597ce4952d3af9004b7ced189f67b1
Eike Hirsch <eike.hirsch@smartsteuer.de>
2016-06-16 18:05:23 +0200

    adds old school text messages and bubbles.

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hauke WebRTC' });
+  res.render('index', { title: 'Chat mit Hauke' });
 });
 
 module.exports = router;
